d5bffaf9b31c32799c3167547bfa9e215a730880
mohamed mhiri <med.mhiri.5@gmail.com>
2017-12-06 12:34:33 +0100

    simplefix in progress

--- fix-server-side/index.py
+++ fix-server-side/index.py
@@ -1,6 +1,15 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+from simplefix import FixMessage
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-  return "Hello, World!"
+    return "Hello, World!"
+
+@app.route("/api/logon")
+def simplefix_init():
+    msg = FixMessage()
+    msg.append_string("8=FIX.4.2")
+    msg.append_pair("9", "")
+    return 'p', 204
